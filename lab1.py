9917150d3b401070f44d4e9802169fb815b4cd5f
kelsey makale <sianwamak@gmail.com>
2020-11-05 11:12:58 +0300

    final changes to lab1

--- lab1.py
+++ lab1.py
@@ -4,22 +4,33 @@
 import re
 
 tokens = []
-code = 'int result = 100;'.split()
+code = 'int result = 100'.split()
+
+RE_Keywords = "auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while|string|class|struc|include"
+RE_Operators = "(\++)|(-)|(=)|(\*)|(/)|(%)|(--)|(<=)|(>=)"
+RE_Numerals = ".[0-9]"
+RE_Special_Characters = "[\[@&~!#$\^\|{}\]:;<>?,\.']|\(\)|\(|\)|{}|\[\]|\""
+RE_Identifiers = "^[a-zA-Z_]+[a-zA-Z0-9_]*"
+RE_Headers = "([a-zA-Z]+\.[h])"
 
 for word in code:
-    if word in ['str', 'int', 'bool']:
-        tokens.append(['DATATYPE', word])
+    if word in RE_Keywords:
+        tokens.append(['KEYWORD', word])
 
-    elif re.match("[a-z]", word) or re.match("[A - Z]", word):
+    elif re.match(RE_Identifiers, word):
         tokens.append(['IDENTIFIER',word])
 
-    elif word in '*-/+%=':
+    elif word in RE_Operators:
         tokens.append(['OPERATOR', word])
 
-    elif re.match(".[0-9]", word):
-        if word[len(word) - 1] == ';':
+    elif re.match(RE_Special_Characters, word):
+        tokens.append(['SPECIAL CHARACTER', word])
+
+    elif re.match(RE_Numerals, word):
             tokens.append(["INTEGER", word[:-1]])
-            tokens.append(['END_STATEMENT', ';'])
-        else:
-            tokens.append(["INTEGER", word])
+
+    else:
+        print("Unkown token key")
+    
+       
 print(tokens)  
